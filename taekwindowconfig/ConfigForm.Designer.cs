11cb721b4b68840736fe8eab5f92ba12cb11fe9c
Thomas ten Cate <ttencate@gmail.com>
2007-12-31 17:10:33 +0000

    added graphics for resize modes

--- taekwindowconfig/ConfigForm.Designer.cs
+++ taekwindowconfig/ConfigForm.Designer.cs
@@ -32,6 +32,8 @@ namespace TaekwindowConfig
 			this.ok = new System.Windows.Forms.Button();
 			this.tabControl = new System.Windows.Forms.TabControl();
 			this.general = new System.Windows.Forms.TabPage();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.startAtLogon = new System.Windows.Forms.CheckBox();
 			this.keysAndButtons = new System.Windows.Forms.TabPage();
@@ -51,6 +53,10 @@ namespace TaekwindowConfig
 			this.leftAlt = new System.Windows.Forms.RadioButton();
 			this.label3 = new System.Windows.Forms.Label();
 			this.resizing = new System.Windows.Forms.TabPage();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label6 = new System.Windows.Forms.Label();
 			this.nineRectangles = new System.Windows.Forms.RadioButton();
 			this.bottomRight = new System.Windows.Forms.RadioButton();
 			this.about = new System.Windows.Forms.TabPage();
@@ -62,19 +68,19 @@ namespace TaekwindowConfig
 			this.title = new System.Windows.Forms.Label();
 			this.logo = new System.Windows.Forms.PictureBox();
 			this.cancel = new System.Windows.Forms.Button();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
 			this.tabControl.SuspendLayout();
 			this.general.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.keysAndButtons.SuspendLayout();
 			this.panel3.SuspendLayout();
 			this.panel1.SuspendLayout();
 			this.panel2.SuspendLayout();
 			this.resizing.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
 			this.about.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// apply
@@ -126,6 +132,30 @@ namespace TaekwindowConfig
 			this.general.TabIndex = 0;
 			this.general.Text = "General";
 			this.general.UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.pictureBox1);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.startAtLogon);
+			this.groupBox1.Location = new System.Drawing.Point(11, 11);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Padding = new System.Windows.Forms.Padding(8, 3, 8, 8);
+			this.groupBox1.Size = new System.Drawing.Size(310, 146);
+			this.groupBox1.TabIndex = 11;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Startup";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(31, 83);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(241, 52);
+			this.pictureBox1.TabIndex = 10;
+			this.pictureBox1.TabStop = false;
 			// 
 			// label1
 			// 
@@ -329,6 +359,10 @@ namespace TaekwindowConfig
 			// 
 			// resizing
 			// 
+			this.resizing.Controls.Add(this.pictureBox3);
+			this.resizing.Controls.Add(this.label7);
+			this.resizing.Controls.Add(this.pictureBox2);
+			this.resizing.Controls.Add(this.label6);
 			this.resizing.Controls.Add(this.nineRectangles);
 			this.resizing.Controls.Add(this.bottomRight);
 			this.resizing.Location = new System.Drawing.Point(4, 22);
@@ -340,10 +374,50 @@ namespace TaekwindowConfig
 			this.resizing.Text = "Resizing";
 			this.resizing.UseVisualStyleBackColor = true;
 			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(30, 211);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(128, 96);
+			this.pictureBox3.TabIndex = 5;
+			this.pictureBox3.TabStop = false;
+			// 
+			// label7
+			// 
+			this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.label7.Location = new System.Drawing.Point(27, 182);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(294, 26);
+			this.label7.TabIndex = 4;
+			this.label7.Text = "The window is resized from the corner or edge that is closest to where you grab i" +
+				"t.";
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(30, 60);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(128, 96);
+			this.pictureBox2.TabIndex = 3;
+			this.pictureBox2.TabStop = false;
+			// 
+			// label6
+			// 
+			this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.label6.Location = new System.Drawing.Point(27, 31);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(294, 26);
+			this.label6.TabIndex = 2;
+			this.label6.Text = "Regardless of where you grab the window, you always resize from its lower right c" +
+				"orner.";
+			// 
 			// nineRectangles
 			// 
 			this.nineRectangles.AutoSize = true;
-			this.nineRectangles.Location = new System.Drawing.Point(11, 34);
+			this.nineRectangles.Location = new System.Drawing.Point(11, 162);
 			this.nineRectangles.Name = "nineRectangles";
 			this.nineRectangles.Size = new System.Drawing.Size(99, 17);
 			this.nineRectangles.TabIndex = 1;
@@ -467,28 +541,6 @@ namespace TaekwindowConfig
 			this.cancel.UseVisualStyleBackColor = true;
 			this.cancel.Click += new System.EventHandler(this.cancel_Click);
 			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(31, 83);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(241, 52);
-			this.pictureBox1.TabIndex = 10;
-			this.pictureBox1.TabStop = false;
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Controls.Add(this.pictureBox1);
-			this.groupBox1.Controls.Add(this.label1);
-			this.groupBox1.Controls.Add(this.startAtLogon);
-			this.groupBox1.Location = new System.Drawing.Point(11, 11);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Padding = new System.Windows.Forms.Padding(8, 3, 8, 8);
-			this.groupBox1.Size = new System.Drawing.Size(310, 146);
-			this.groupBox1.TabIndex = 11;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "Startup";
-			// 
 			// ConfigForm
 			// 
 			this.AcceptButton = this.apply;
@@ -508,6 +560,9 @@ namespace TaekwindowConfig
 			this.Text = "Configuration";
 			this.tabControl.ResumeLayout(false);
 			this.general.ResumeLayout(false);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.keysAndButtons.ResumeLayout(false);
 			this.keysAndButtons.PerformLayout();
 			this.panel3.ResumeLayout(false);
@@ -515,12 +570,11 @@ namespace TaekwindowConfig
 			this.panel2.ResumeLayout(false);
 			this.resizing.ResumeLayout(false);
 			this.resizing.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
 			this.about.ResumeLayout(false);
 			this.about.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			this.groupBox1.ResumeLayout(false);
-			this.groupBox1.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -563,6 +617,10 @@ namespace TaekwindowConfig
 		private System.Windows.Forms.Button cancel;
 		private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.PictureBox pictureBox3;
 	}
 }
 
