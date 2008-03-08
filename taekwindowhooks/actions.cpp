8dc7328090663e92686974e093459f81839ec38b
Thomas ten Cate <ttencate@gmail.com>
2008-03-08 17:49:16 +0000

    better explanation comment

--- taekwindowhooks/actions.cpp
+++ taekwindowhooks/actions.cpp
@@ -7,13 +7,14 @@ void doPushBack(HWND window) {
 	// the active window is still deactivated, because Windows somehow sends it a WM_NCACTIVATE message.
 	// Hence the following hack: restore the focus of the previous application after the SetWindowPos() call.
 	// But alas, since we're in the middle of a focus change, GetForegroundWindow() and GetGUIThreadInfo
-	// both give NULL for this window handle...
+	// both give NULL for this window handle... so we keep track of the last known active window in the global
+	// lastForegroundWindow, updated upon every mouse movement. Ugh.
 
 	// Push the window to the back.
 	SetWindowPos(window, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOSIZE);
 
-	// Reactivate the previous window.
-	if (lastForegroundWindow) {
+	// Reactivate the previous window, which pulls it to the front; so change its position in the Z order back.
+	if (lastForegroundWindow && lastForegroundWindow != window) {
 		DEBUGLOG("Activating previously active window 0x%X", lastForegroundWindow);
 		SetForegroundWindow(lastForegroundWindow);
 	}
