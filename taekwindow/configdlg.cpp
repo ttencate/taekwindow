1790456d85434977fe2d4553385054a44a8640ba
Thomas ten Cate <ttencate@gmail.com>
2009-08-30 19:30:09 +0000

    Now displaying PNG picture in the configuration dialog, using GDI+.

--- taekwindow/configdlg.cpp
+++ taekwindow/configdlg.cpp
@@ -7,14 +7,22 @@
 #include <windows.h>
 #include <commctrl.h>
 #include <tchar.h>
+#include <gdiplus.h>
+
+// TODO remove
+#include <stdio.h>
 
 /* Handle of the configuration dialog's window.
  */
 HWND configWindowHandle = 0;
 
-void hyperlinkClicked(LONG_PTR controlID) {
-	// TODO
-}
+/* GDI+ token.
+ */
+ULONG_PTR gdiplusToken;
+
+enum ImageID { LOGO_IMAGE, NUM_IMAGES };
+const int IMAGE_RESOURCE_IDS[NUM_IMAGES] = { IDB_LOGO };
+Gdiplus::Image *images[NUM_IMAGES];
 
 BOOL CALLBACK defaultDialogProc(HWND dialogHandle, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch (message) {
@@ -23,6 +31,22 @@ BOOL CALLBACK defaultDialogProc(HWND dialogHandle, UINT message, WPARAM wParam, 
 		default:
 			return FALSE;
 	}
+}
+
+void drawImage(DRAWITEMSTRUCT *item, ImageID imageID) {
+	if (images[imageID]) {
+		Gdiplus::Image *image = images[imageID];
+		RECT &rect = item->rcItem;
+		int x = (rect.left + rect.right - image->GetWidth()) / 2;
+		int y = (rect.top + rect.bottom - image->GetHeight()) / 2;
+		HDC dc = item->hDC;
+		Gdiplus::Graphics graphics(dc);
+		graphics.DrawImage(image, x, y);
+	}
+}
+
+void hyperlinkClicked(LONG_PTR controlID) {
+	// TODO
 }
 
 BOOL CALLBACK aboutPageDialogProc(HWND dialogHandle, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -35,6 +59,12 @@ BOOL CALLBACK aboutPageDialogProc(HWND dialogHandle, UINT message, WPARAM wParam
 			SetDlgItemText(dialogHandle, IDC_APPEMAIL, _T("<a>") _T(APPLICATION_EMAIL) _T("</a>"));
 			SetDlgItemText(dialogHandle, IDC_APPWEBSITE, _T("<a>") _T(APPLICATION_WEBSITE) _T("</a>"));
 			break;
+		case WM_DRAWITEM:
+			if (wParam == IDC_APPLOGO) {
+				drawImage((DRAWITEMSTRUCT*)lParam, LOGO_IMAGE);
+				return TRUE;
+			}
+			break;
 		case WM_COMMAND:
 			hyperlinkClicked(GetWindowLongPtr(dialogHandle, GWLP_ID));
 			return TRUE;
@@ -42,16 +72,150 @@ BOOL CALLBACK aboutPageDialogProc(HWND dialogHandle, UINT message, WPARAM wParam
 	return defaultDialogProc(dialogHandle, message, wParam, lParam);
 }
 
-void showConfig() {
+/* Initializes the Common Controls library.
+ * It is ok to call this multiple times, and they do not need shutdown.
+ */
+bool initCommonControls() {
 	// Huhhuh, init common control sex. Allcaps break my head.
 	INITCOMMONCONTROLSEX icc;
 	icc.dwSize = sizeof(INITCOMMONCONTROLSEX);
-	icc.dwICC = ICC_STANDARD_CLASSES; // TODO ICC_LINK_CLASS
-	InitCommonControlsEx(&icc);
+	icc.dwICC = ICC_STANDARD_CLASSES | ICC_LINK_CLASS;
+	if (InitCommonControlsEx(&icc) != TRUE) {
+		showLastError(_T("Unable to initialize common controls"));
+		return false;
+	}
+	return true;
+}
+
+/* Initializes GDI+.
+ * Returns true on success.
+ * If it returns false, the error will already have been reported to the user.
+ */
+bool initGdiplus() {
+	// Assuming that 0 is an invalid token, but this is not documented...
+	gdiplusToken = 0;
+
+	Gdiplus::GdiplusStartupInput gsi;
+	gsi.GdiplusVersion = 1;
+	gsi.DebugEventCallback = NULL;
+	gsi.SuppressBackgroundThread = FALSE;
+	gsi.SuppressExternalCodecs = TRUE;
+	Gdiplus::Status status = Gdiplus::GdiplusStartup(&gdiplusToken, &gsi, NULL);
+	if (status != Gdiplus::Ok) {
+		showError(_T("Could not initialize GDI+"), _T("The GDI+ graphics library could not be initialized. Some images will not be visible.\n\nGdiplusStartup returned the error code %1!d!."), status);
+		return false;
+	}
+	return true;
+}
+
+void shutdownGdiplus() {
+	// Assuming that 0 is an invalid token, but this is not documented...
+	if (gdiplusToken) {
+		Gdiplus::GdiplusShutdown(gdiplusToken);
+	}
+}
+
+/* Loads the PNG images from resources, storing them in the images array.
+ * Silently fails for images that cannot be loaded, storing NULL for that image.
+ */
+void loadImages() {
+	for (int i = 0; i < NUM_IMAGES; ++i) {
+		images[i] = NULL;
+
+		// Find and load the resource from the executable.
+		// It has the undocumented type "PNG".
+		int resourceID = IMAGE_RESOURCE_IDS[i];
+		HRSRC resourceHandle = FindResource(NULL, MAKEINTRESOURCE(resourceID), L"PNG");
+		if (!resourceHandle) {
+			debugShowLastError(_T("Could not find resource"));
+			continue;
+		}
+		HGLOBAL resource = LoadResource(NULL, resourceHandle);
+		if (!resource) {
+			debugShowLastError(_T("Could not load resource"));
+			continue;
+		}
+		void *resourceData = LockResource(resource);
+		if (!resourceData) {
+			debugShowLastError(_T("Could not lock resource"));
+			continue;
+		}
+
+		// Allocate memory to copy the resource to.
+		// Unfortunately, the HGLOBAL returned by LoadResource is not really a HGLOBAL,
+		// so we cannot have CreateStreamOnHGlobal read from that. We have to make a copy.
+
+		// Note: passing NULL to SizeofResource is not explicitly allowed by the docs,
+		// but the other XxxResource functions do allow it, and much code on the internet also passes NULL.
+		// So let's assume that this is an omission in the docs.
+		DWORD bytes = SizeofResource(NULL, resourceHandle);
+		if (!bytes) {
+			debugShowLastError(_T("Could not determine resource size"));
+			continue;
+		}
+		HGLOBAL mem = GlobalAlloc(GMEM_MOVEABLE, bytes);
+		if (!mem) {
+			debugShowLastError(_T("Could not allocate memory"));
+			continue;
+		}
+		void *memData = GlobalLock(mem);
+		if (!memData) {
+			debugShowLastError(_T("Could not lock memory"));
+			continue;
+		}
+		if (!CopyMemory(memData, resourceData, bytes)) {
+			debugShowLastError(_T("Could not copy memory"));
+			continue;
+		}
+		if (!GlobalUnlock(mem) && GetLastError()) {
+			debugShowLastError(_T("Could not unlock memory"));
+			continue;
+		}
+
+		// Create a stream from the memory.
+		// Note that by passing TRUE, the stream calls GlobalFree automatically.
+		IStream *stream;
+		if (CreateStreamOnHGlobal(mem, TRUE, &stream) != S_OK) {
+			debugShowLastError(_T("Could not create image stream"));
+			GlobalFree(mem);
+			continue;
+		}
+		// Stream might be bigger than resource, don't know whether PNG loader minds,
+		// but better be safe.
+		ULARGE_INTEGER largeBytes;
+		largeBytes.HighPart = 0;
+		largeBytes.LowPart = bytes;
+		stream->SetSize(largeBytes);
+
+		// Create an image from the stream.
+		images[i] = new Gdiplus::Image(stream);
+
+		// Clean up.
+		stream->Release();
+	}
+}
+
+void unloadImages() {
+	for (int i = 0; i < NUM_IMAGES; ++i) {
+		if (images[i]) {
+			delete images[i];
+			images[i] = NULL;
+		}
+	}
+}
+
+void showConfig() {
+	// Most of the runs, the user will not use the configuration dialog.
+	// That's why we do not initialize in WinMain, but only when it is used.
+	if (!initCommonControls()) {
+		return;
+	}
+	initGdiplus(); // we CAN work without GDI+, so ignore any errors
+	loadImages();
 
 	const int NUM_PAGES = 1;
-	const int pageTemplates[NUM_PAGES] = { IDD_ABOUTPAGE };
-	const DLGPROC pageDialogProcs[NUM_PAGES] = { &aboutPageDialogProc };
+	const int PAGE_TEMPLATES[NUM_PAGES] = { IDD_ABOUTPAGE };
+	const DLGPROC PAGE_DIALOG_PROCS[NUM_PAGES] = { &aboutPageDialogProc };
 
 	PROPSHEETPAGE pages[NUM_PAGES];
 	for (int i = 0; i < NUM_PAGES; ++i) {
@@ -59,10 +223,10 @@ void showConfig() {
 		page.dwSize = sizeof(PROPSHEETPAGE);
 		page.dwFlags = 0;
 		page.hInstance = getCurrentInstance();
-		page.pszTemplate = MAKEINTRESOURCE(pageTemplates[i]);
+		page.pszTemplate = MAKEINTRESOURCE(PAGE_TEMPLATES[i]);
 		page.hIcon = NULL;
 		page.pszTitle = NULL; // fetched from dialog resource
-		page.pfnDlgProc = pageDialogProcs[i];
+		page.pfnDlgProc = PAGE_DIALOG_PROCS[i];
 		page.lParam = 0;
 		page.pfnCallback = NULL;
 		page.pcRefParent = NULL;
@@ -88,6 +252,9 @@ void showConfig() {
 	header.pszbmHeader = NULL;
 
 	PropertySheet(&header);
+
+	unloadImages();
+	shutdownGdiplus();
 }
 
 bool isConfigShowing() {
--- taekwindow/util.hpp
+++ taekwindow/util.hpp
@@ -3,8 +3,23 @@
 
 #include <windows.h>
 
-/* Shows a messagebox with the message corresponding to GetLastError(), with the given title.
+/* Shows a message box with the given message, in which argument substitution is done
+ * according to the format of FormatMessage (not printf syntax!).
+ */
+void showError(LPCWSTR title, LPCWSTR text, ...);
+
+/* Shows a message box with the message corresponding to GetLastError(), with the given title.
  */
 void showLastError(LPCWSTR title);
+
+/* Only calls showLastError in debug mode.
+ * Useful for checking cases that are very unlikely to go wrong once they go right,
+ * but that we want to check during debugging anyway.
+ */
+#ifdef DEBUG
+#	define debugShowLastError(title) showLastError(title)
+#else
+#	define debugShowLastError(title)
+#endif
 
 #endif
