11936c8c15ec197c9fdba4866f5c66718ac8f681
Thomas ten Cate <ttencate@gmail.com>
2009-10-22 18:42:16 +0000

    Fixed bug in error message display.

--- taekwindow/util.cpp
+++ taekwindow/util.cpp
@@ -6,7 +6,7 @@ void showError(HWND parent, LPCWSTR title, LPCWSTR text, ...) {
 	PVOID msg;
 	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_STRING, text, 0, 0, (LPWSTR)&msg, 0, &list);
 	va_end(list);
-	MessageBox(NULL, (LPCWSTR)msg, title, MB_OK | MB_ICONERROR);
+	MessageBox(parent, (LPCWSTR)msg, title, MB_OK | MB_ICONERROR);
 	LocalFree(msg);
 }
 
